5334a81210af4d517b169ff7898d00a2ff3c4420
nguyendo2008 <Nguyendo2008@gmail.com>
2021-02-26 02:19:45 +0700

    update content

--- src/views/ProductDetailPage.vue
+++ src/views/ProductDetailPage.vue
@@ -36,7 +36,7 @@
           >
             <div class="d-flex align-items-center justify-content-center">
               <b-img
-                :src="product.imageUrl != null && product.imageUrl.lenght > 0 ? defaultImage : defaultImage"
+                :src="product.imageUrl != null && product.imageUrl.length > 0 ? defaultImage : defaultImage"
                 :alt="`Image of ${product.name}`"
                 class="product-img"
                 fluid
